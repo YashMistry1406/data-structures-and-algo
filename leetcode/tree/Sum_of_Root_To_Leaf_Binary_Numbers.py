9e79bddf30a24af9e17bfe9bc8d66d8d1c00aa7f
YashMistry1406 <yashmistry2000@gmail.com>
2021-01-30 14:26:25 +0530

    bit operation and iterative solution for sum of root

--- leetcode/tree/Sum_of_Root_To_Leaf_Binary_Numbers.py
+++ leetcode/tree/Sum_of_Root_To_Leaf_Binary_Numbers.py
@@ -1,3 +1,28 @@
+    #ITERATIVE SOLUTION 
+#class Solution:
+#    def sumRootToLeaf(self, root: TreeNode) -> int:
+#        
+#        # pre-order transveral iterative solution
+#        # This is the same question as Leetcode #144 Binary Tree Preorder Traversal
+#        
+#        stack=[] # store a tuple of (root, value of current path)
+#        value = 0 # need an imutable variable to store the value for each leaves
+#        result=0
+#        
+#        while root or stack:
+#            if root:
+#                value = (value<<1)+root.val #update the current value
+#                stack.append((root,value)) # put the tuple into a stack  
+#                if not root.left and not root.right: # iff left, right are both none, it is a valid leaf
+#                    result += value
+#                root = root.left
+#                    
+#            else:
+#                (root,value) = stack.pop()
+#                root=root.right
+#        
+#        return result
+#
 #You are given the root of a binary tree where each node has a value 0 or 1.
 #Each root-to-leaf path represents a binary number starting with the most significant bit.
 #For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.

ea8c967c19c43e265e15acc3e3ac0456e277c8a0
YashMistry1406 <yashmistry2000@gmail.com>
2021-02-12 16:28:16 +0530

    recover tree from preorder traversal

---
+++ leetcode/tree/recover_tree_from_preorder.py
@@ -0,0 +1,37 @@
+#Explanation
+#We save the construction path in a stack.
+#In each loop,
+#we get the number level of '-'
+#we get the value val of node to add.
+#
+#If the size of stack is bigger than the level of node,
+#we pop the stack until it's not.
+#
+#Finally we return the first element in the stack, as it's root of our tree.
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def recoverFromPreorder(self, S):
+        stack, i = [], 0
+        while i < len(S):
+            level, val = 0, ""
+            while i < len(S) and S[i] == '-':
+                level, i = level + 1, i + 1
+            while i < len(S) and S[i] != '-':
+                val, i = val + S[i], i + 1
+            while len(stack) > level:
+                stack.pop()
+            node = TreeNode(val)
+            if stack and stack[-1].left is None:
+                stack[-1].left = node
+            elif stack:
+                stack[-1].right = node
+            stack.append(node)
+        return stack[0]
+        
+        
+

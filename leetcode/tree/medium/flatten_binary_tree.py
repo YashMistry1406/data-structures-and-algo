1bb280f34ffe3ef457b1a3c1cd7e7c3c89b97daa
YashMistry1406 <yashmistry2000@gmail.com>
2021-02-23 15:04:53 +0530

    path sum type

---
+++ leetcode/tree/medium/flatten_binary_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        self.stack=[]
+        
+        if not root:
+            return
+        self.prev = root
+        self.flatten(root.left)
+
+        temp = root.right
+        root.right, root.left = root.left, None
+        self.prev.right = temp
+
+        self.flatten(temp)

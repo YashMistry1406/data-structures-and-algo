6037950dab6fc55328a824a486ee7249c91f2cd1
YashMistry1406 <yashmistry2000@gmail.com>
2021-03-02 15:28:13 +0530

    hashing

---
+++ hashing/dict.py
@@ -0,0 +1,11 @@
+di={1:["idli","dosa","poha"],2:["bata vada","puri bhaji"],3:["thali","pav bhaji"]}
+for i,j in di.items():
+    print(i,":",j)
+dic={}
+di[1].append("upma")
+#inserting a value in list value of a key 
+print(di[1])
+print(di)
+#print(len(di))
+#print(di.get(2))
+#print(di[1][1])

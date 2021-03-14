4e289edf8fd61c47f4ff51ea0d6b41c92bc39589
YashMistry1406 <yashmistry2000@gmail.com>
2021-03-14 14:17:50 +0530

    dynamic prog

---
+++ dynamic programming/traveller.py
@@ -0,0 +1,16 @@
+def gridTraveller(m,n,memory={}):
+    key=str(m) +','+str(n)
+    print(memory)
+    if key in memory:
+        return memory[key]
+    if m==1 and n==1:
+        return 1
+    if m==0 or n==0:
+        return 0
+
+    memory[key]=(gridTraveller(m-1,n,memory)+gridTraveller(m,n-1,memory))
+    return memory[key]
+if __name__ == "__main__" :
+    m,n=map(int,input().split())
+    print(gridTraveller(m,n))
+

d7dc8000ef37f7ba338be2c40dbf84f904d950df
YashMistry1406 <yashmistry2000@gmail.com>
2021-04-28 12:03:02 +0530

    equal sum partition done

--- dynamic programming/learning_dp/equal_sum_partition.py
+++ dynamic programming/learning_dp/equal_sum_partition.py
@@ -1,10 +1,37 @@
-def solve(arr):
-    return True
+class Solution:
+    def canPartition(self, arr: List[int]) -> bool:
+        sum = 0
+        i, j = 0, 0
+        n = len(arr)
 
+        # calculate sum of all elements
+        for i in range(n):
+            sum += arr[i]
 
-stack=list(map(int,input().split()))
+        if sum % 2 != 0:
+            return False
 
-if sum(stack)%2 !=0:
-    print("False")
-else:
-    solve(stack)
+        part = [[True for i in range(n + 1)]
+                for j in range(sum // 2 + 1)]
+
+        # initialize top row as true
+        for i in range(0, n + 1):
+            part[0][i] = True
+
+        # initialize leftmost column,
+        # except part[0][0], as 0
+        for i in range(1, sum // 2 + 1):
+            part[i][0] = False
+
+        # fill the partition table in
+        # bottom up manner
+        for i in range(1, sum // 2 + 1):
+
+            for j in range(1, n + 1):
+                part[i][j] = part[i][j - 1]
+
+                if i >= arr[j - 1]:
+                    part[i][j] = (part[i][j] or
+                                  part[i - arr[j - 1]][j - 1])
+
+        return part[sum // 2][n]

b457e394a1845a52fa35ec8ddc91abfe5cd3ea5d
YashMistry1406 <yashmistry2000@gmail.com>
2021-07-01 22:19:10 +0530

    check

--- learning kotlin/tp.kt
+++ learning kotlin/tp.kt
@@ -1,3 +1,16 @@
-class tp(val num:Int){
-}
+import java.util.Scanner
+import kotlin.collections.mutableListOf
+fun main(){
+    val sc=Scanner(System.`in`)
+
+    val user=arrayOf(1,2,3,4,5)
+    val user_set=mutableListOf(9,8,7,6,5,4)
+    var len=user_set.size
+    println("$len")
+    for (i in 0..len-1)
+    {
+
+        println(user_set[i])
+    }
+    }
 
